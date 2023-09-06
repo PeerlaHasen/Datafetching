0de2f36638547102b833f97eeb571837d5d04aac
peerla hasen <peerla.hasen@accionlabs.com>
2023-09-06 15:55:00 +0530

    added next js with redux

--- pages/content.js
+++
@@ -1,7 +0,0 @@
-const content = ()=>{
-    return(
-        <h1>welcome to next js </h1>
-    )
-}
-
-export default content;
---
+++ pages/redux/actions.js
@@ -0,0 +1,13 @@
+// redux/actions.js
+
+export const increment = () => {
+  return {
+    type: 'INCREMENT',
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: 'DECREMENT',
+  };
+};
